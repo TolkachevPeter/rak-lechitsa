7ba0b9a0cf82db965f61319a210b89e8b4aa531a
alexander.b <gituser1056@gmail.com>
2020-05-06 20:20:45 +0300

    сделана структура для компонентов (11)

--- pages/index.vue
+++ pages/index.vue
@@ -2,36 +2,31 @@
   <div class="container">
     <div>
       <logo />
-      <h1 class="title">
-        rak-lechitsa
-      </h1>
-      <h2 class="subtitle">
-        My prime Nuxt.js project
-      </h2>
+      <h1 class="title">rak-lechitsa</h1>
+      <h2 class="subtitle">My prime Nuxt.js project</h2>
       <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green"
+          >Documentation</a
+        >
         <a
           href="https://github.com/nuxt/nuxt.js"
           target="_blank"
           class="button--grey"
+          >GitHub</a
         >
-          GitHub
-        </a>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue';
+/*import Logo from '~/components/Logo.vue';
 
 export default {
   components: {
     Logo,
   },
-};
+};*/
 </script>
 
 <style>
