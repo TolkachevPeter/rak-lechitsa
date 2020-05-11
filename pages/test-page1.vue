1159fc417c8a29fe580fe67dc976eeb95eda7109
Alexander1056 <gituser1056@gmail.com>
2020-05-11 15:35:33 +0300

    Changed default.vue, nuxt.config.js

---
+++ pages/test-page1.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <!-- <my-header></my-header> -->
+    <my-banner></my-banner>
+  </div>
+</template>
+
+<script>
+//import Header from '@/components/Header';
+import Banner from '@/components/Banner';
+export default {
+  /*  compponents: {
+    'my-header': Header,
+  },
+  */
+  components: {
+    'my-banner': Banner,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
