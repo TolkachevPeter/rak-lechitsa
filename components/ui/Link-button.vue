2e6fc4a3e8546fe8567db83a5c1a8dec2da04806
tony-developer <anton.serebriackoff@yandex.ru>
2020-05-11 14:01:42 +0300

    перезалил, в прошлый раз link-button не добавил

---
+++ components/ui/Link-button.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="link-button">
+    <a class="link-button__text" href="URL">Больше статей</a>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.link-button {
+  max-width: 1320px;
+  height: 82px;
+  margin: 0 60px;
+  background: #fbfbfb;
+  display: flex;
+  justify-content: center;
+}
+
+.link-button__text {
+  font-family: Inter;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 20px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  color: #000000;
+  text-decoration: none;
+}
+</style>
