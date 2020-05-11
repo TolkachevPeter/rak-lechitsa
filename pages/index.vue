32fa482f8cfa1520d49ec7cdb6393569ce9afad3
AntonZ <antonzm34@gmail.com>
2020-05-11 11:58:30 +0400

    Сверстал popup

--- pages/index.vue
+++ pages/index.vue
@@ -20,13 +20,13 @@
 </template>
 
 <script>
-/*import Logo from '~/components/Logo.vue';
+// import Logo from '~/components/Logo.vue';
 
 export default {
-  components: {
-    Logo,
-  },
-};*/
+  // components: {
+  //   logo: Logo,
+  // },
+};
 </script>
 
 <style>
