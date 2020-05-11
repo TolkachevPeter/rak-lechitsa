a4b74e4fe9d0935f29fc3552b51f28b1d0c0b4fc
DK <13776265+i386net@users.noreply.github.com>
2020-05-11 12:29:09 +0300

    header

---
+++ components/Header.vue
@@ -0,0 +1,61 @@
+<template>
+  <header class="header">
+    <h2 class="header__title">
+      Проект Благотворительного Фонда Константина Хабенского
+    </h2>
+    <div class="header__links">
+      <header-nav />
+      <header-button @click="" class="header__button"
+        >Рассказать историю</header-button
+      >
+    </div>
+  </header>
+</template>
+
+<script>
+import Button from '~/components/ui/Button';
+import Menu from '~/components/Menu';
+export default {
+  components: {
+    'header-button': Button,
+    'header-nav': Menu,
+  },
+};
+</script>
+
+<style scoped>
+.header {
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 1440px;
+  width: 100%;
+  padding: 18px 4%;
+  border-bottom: 1px solid #e8e8e8;
+}
+.header__title {
+  font-size: 16px;
+  line-height: 20px;
+  font-weight: 600;
+  max-width: 340px;
+}
+.header__links {
+  display: flex;
+  align-items: center;
+}
+.header__button {
+  padding: 0;
+  margin-left: 40px;
+  font-size: 18px;
+  line-height: 24px;
+  background-color: transparent;
+  border: none;
+}
+.header__button:hover {
+  cursor: pointer;
+}
+.header >>> .nav__link_underline {
+  border-bottom: 1px solid black;
+}
+</style>
