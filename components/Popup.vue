74094a09b3761bf9df092b7d0e2011d3105c679b
Alexander1056 <gituser1056@gmail.com>
2020-05-12 10:24:13 +0300

    Added About block & component and some fixes

--- components/Popup.vue
+++ components/Popup.vue
@@ -24,7 +24,6 @@
         <p class="popup__policy" :class="{ showPolicy: needPolicy }">
           {{ policyText }}
 
-
           <a href="../policy" class="popup__policy-link">{{ policyLink }}</a>
         </p>
       </div>
