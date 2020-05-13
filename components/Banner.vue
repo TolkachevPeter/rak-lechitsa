ac98c06e48a541db817d9d358ea330f8b79c54ac
AntonZ <antonzm34@gmail.com>
2020-05-13 21:28:23 +0400

    banner

--- components/Banner.vue
+++ components/Banner.vue
@@ -28,12 +28,46 @@ export default {
   background-color: #613a93;
   display: flex;
   flex-direction: column;
-  margin: 0 auto;
+  /* margin: 0 auto; */
   align-items: center;
+  justify-content: center;
+  position: relative;
 }
 .banner-title {
   color: #ffffff;
-  margin-top: 290px;
+  /* margin-top: 290px; */
   text-align: center;
+}
+@media (max-width: 1280px) {
+  .title {
+    font-size: 78px;
+    line-height: 94px;
+  }
+  .banner {
+    min-height: 620px;
+  }
+}
+@media (max-width: 1024px) {
+  .banner {
+    min-height: 540px;
+  }
+}
+@media (max-width: 768px) {
+  .title {
+    font-size: 64px;
+    line-height: 77px;
+  }
+  .banner {
+    min-height: 780px;
+  }
+}
+@media (max-width: 720px) {
+  .title {
+    font-size: 36px;
+    line-height: 44px;
+  }
+  .banner {
+    min-height: 480px;
+  }
 }
 </style>
