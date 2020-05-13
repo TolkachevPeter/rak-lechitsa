7a805a81d1b0b02ad7ce6ca47a9d197f714b1acb
Alexander1056 <gituser1056@gmail.com>
2020-05-13 09:17:20 +0300

    Создал папку tpgs и поместил туда тестовые страницы, отредактировал .gitignore файл

---
+++ pages/stories/_id.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <sectiontitle
+      >Александр Тарханов: «Я не могу победить свою пунктуальность в отличии от
+      рака»</sectiontitle
+    >
+  </div>
+</template>
+
+<script>
+import SectionTitle from '@/components/ui/SectionTitle';
+
+export default {
+  components: {
+    sectiontitle: SectionTitle,
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  width: 100%;
+  height: 100vh;
+  padding: 200px;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+}
+</style>
---
+++ pages/stories/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <sectiontitle>Истории неизлечимых привычек</sectiontitle>
+    <linkbutton :url="'1'" class="linkbutton_stories"
+      >Cсылка на историю 1</linkbutton
+    >
+  </div>
+</template>
+
+<script>
+import SectionTitle from '@/components/ui/SectionTitle';
+import Linkbutton from '@/components/ui/Link-button';
+
+export default {
+  components: {
+    sectiontitle: SectionTitle,
+    linkbutton: Linkbutton,
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  width: 100%;
+  height: 100vh;
+  padding: 200px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+}
+.linkbutton_stories {
+  background: white;
+}
+</style>
