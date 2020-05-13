b68cf59395c99672704efd253fb54522e6944368
DK <13776265+i386net@users.noreply.github.com>
2020-05-13 13:23:40 +0300

    refactor stories

--- components/Story-elem.vue
+++ components/Story-elem.vue
@@ -1,68 +1,17 @@
 <template>
   <section class="stories">
     <h2 class="stories__header">Истории неизлечимых привычек</h2>
-    <div class="stories stories-list">
-      <div class="story-element">
-        <img class="story-element__photo" src alt="Фото человека с историей" />
-        <span class="story-element__name">Владимир Тен</span>
-        <span class="story-element__text"
-          >Я всегда читаю книги с конца, - и это не лечится, в отличие от
-          рака.</span
-        >
-      </div>
-      <div class="story-element">
-        <img class="story-element__photo" src alt="Фото человека с историей" />
-        <span class="story-element__name">Владимир Познер</span>
-        <span class="story-element__text"
-          >Я боюсь акул — и, в отличии от рака, это не лечится.</span
-        >
-      </div>
-      <div class="story-element">
-        <img class="story-element__photo" src alt="Фото человека с историей" />
-        <span class="story-element__name">Александр Тарханов</span>
-        <span class="story-element__text"
-          >Я не могу победить свою пунктуальность в отличии от рака.</span
-        >
-      </div>
-      <div class="story-element">
-        <img class="story-element__photo" src alt="Фото человека с историей" />
-        <span class="story-element__name">Владимир Тен</span>
-        <span class="story-element__text"
-          >Я всегда читаю книги с конца, - и это не лечится, в отличие от
-          рака.</span
-        >
-      </div>
-      <div class="story-element">
-        <img class="story-element__photo" src alt="Фото человека с историей" />
-        <span class="story-element__name">Владимир Тен</span>
-        <span class="story-element__text"
-          >Я всегда читаю книги с конца, - и это не лечится, в отличие от
-          рака.</span
-        >
-      </div>
-      <div class="story-element">
-        <img class="story-element__photo" src alt="Фото человека с историей" />
-        <span class="story-element__name">Владимир Познер</span>
-        <span class="story-element__text"
-          >Я боюсь акул — и, в отличии от рака, это не лечится.</span
-        >
-      </div>
-      <div class="story-element">
-        <img class="story-element__photo" src alt="Фото человека с историей" />
-        <span class="story-element__name">Александр Тарханов</span>
-        <span class="story-element__text"
-          >Я не могу победить свою пунктуальность в отличии от рака.</span
-        >
-      </div>
-      <div class="story-element">
-        <img class="story-element__photo" src alt="Фото человека с историей" />
-        <span class="story-element__name">Владимир Тен</span>
-        <span class="story-element__text"
-          >Я всегда читаю книги с конца, - и это не лечится, в отличие от
-          рака.</span
-        >
-      </div>
-    </div>
+    <ul class="stories stories-list">
+      <li class="story-element" v-for="story in stories" :key="story.id">
+        <img
+          class="story-element__photo"
+          v-bind:src="story.photo"
+          v-bind:alt="story.photo_alt"
+        />
+        <span class="story-element__name">{{ story.name }}</span>
+        <span class="story-element__text">{{ story.story_text }}</span>
+      </li>
+    </ul>
   </section>
 </template>
 
