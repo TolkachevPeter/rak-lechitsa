4a2dadb80570a26619dd1690dd2a2b2c7d8a4d96
Tolkachev Peter <peter.tolkachev@gmail.com>
2020-05-15 23:03:09 +0300

    Footer New Version

--- components/Footer.vue
+++ components/Footer.vue
@@ -6,55 +6,69 @@
           Спасибо всем, кто помог состояться этому проекту
         </h3>
         <div class="footer__column">
-          <nav class="menu">
-            <ul class="menu__items">
-              <li class="menu__item">
-                <a href="#" class="menu__link">Главная</a>
-              </li>
-              <li class="menu__item">
-                <a href="#" class="menu__link">Истории</a>
-              </li>
-            </ul>
-          </nav>
+          <footer-nav class="footer-nav"></footer-nav>
         </div>
 
         <div class="footer__column footer__column_content-info">
-          <nav class="menu">
-            <ul class="menu__items menu__items_column">
-              <li class="menu__item">
-                Мы в
-                <a
-                  href="https://www.instagram.com/raklechitsa/"
-                  target="_blank"
-                  class="menu__link menu__link_social"
-                  >Инстаграме</a
-                >
-                и
-                <a
-                  href="https://www.youtube.com/results?search_query=%23%D1%8D%D1%82%D0%BE%D0%BD%D0%B5%D0%BB%D0%B5%D1%87%D0%B8%D1%82%D1%81%D1%8F"
-                  target="_blank"
-                  class="menu__link menu__link_social"
-                  >Youtube</a
-                >
-              </li>
-              <!-- Тут будет попап, но его еще нет в макете -->
-              <li class="menu__item">
-                <a href="#" target="_blank" class="menu__link">Поделитесь ↗</a>
-              </li>
-            </ul>
-          </nav>
+          <ul class="footer__column-links">
+            <li class="footer__list-item">
+              Мы в
+              <a
+                href="https://www.instagram.com/raklechitsa/"
+                target="_blank"
+                class="footer__link footer__link_social"
+                >Инстаграме</a
+              >
+              и
+              <a
+                href="https://www.youtube.com/results?search_query=%23%D1%8D%D1%82%D0%BE%D0%BD%D0%B5%D0%BB%D0%B5%D1%87%D0%B8%D1%82%D1%81%D1%8F"
+                target="_blank"
+                class="footer__link footer__link_social"
+                >Youtube</a
+              >
+            </li>
+            <!-- Тут будет попап, но его еще нет в макете -->
+            <li class="footer__list-item">
+              <a href="#" target="_blank" class="footer__link"
+                >Поделитесь &#8599;</a
+              >
+            </li>
+          </ul>
         </div>
       </div>
       <div class="footer__copyrights">
-        <p class="footer__project">Рак Лечится 2020</p>
-        <p class="footer__copyright">Сделано студентами Яндекс Практикум</p>
+        <p class="footer__project">Рак Лечится {{ localeDate }}</p>
+        <p class="footer__copyright">
+          Сделано студентами
+          <a
+            href="https://praktikum.yandex.ru/"
+            target="_blank"
+            class="footer__copyright-link"
+            >Яндекс Практикум</a
+          >
+        </p>
       </div>
     </div>
   </footer>
 </template>
 
 <script>
-export default {};
+import Menu from '~/components/Menu';
+export default {
+  components: {
+    'footer-nav': Menu,
+  },
+  // Тут я не знаю, нашел на хабре, немного переделал
+  data: () => ({
+    date: 1580558031264,
+  }),
+
+  computed: {
+    localeDate() {
+      return new Date(this.date).getFullYear();
+    },
+  },
+};
 </script>
 
 <style scoped>
@@ -90,16 +104,14 @@ export default {};
   position: relative;
 }
 
-.menu {
-  font-family: Inter;
-}
-.menu__items {
+.footer__column-links {
   display: flex;
   list-style: none;
+  flex-direction: column;
   /* padding-inline-start: 0; */
 }
 
-.menu__item {
+.footer__list-item {
   padding-right: 40px;
   padding-bottom: 44px;
   font-family: Inter;
@@ -110,10 +122,7 @@ export default {};
   color: #121212;
 }
 
-.menu__items_column {
-  flex-direction: column;
-}
-.menu__link {
+.footer__link {
   font-family: Inter;
   font-style: normal;
   font-weight: normal;
@@ -123,11 +132,11 @@ export default {};
   text-decoration: none;
 }
 
-.menu__link:hover {
+.footer__link:hover {
   opacity: 0.7;
 }
 
-.menu__link_social {
+.footer__link_social {
   text-decoration: underline;
 }
 
@@ -157,6 +166,17 @@ export default {};
   line-height: 100%;
   color: #898989;
 }
+
+.footer__copyright-link {
+  font-family: Inter;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 18px;
+  line-height: 100%;
+  color: #898989;
+  text-decoration: none;
+}
+
 .container {
   margin: 0 auto;
   max-width: 1320px;
@@ -175,7 +195,7 @@ export default {};
     font-size: 28px;
     line-height: 32px;
   }
-  .menu__item {
+  .footer__list-item {
     font-size: 16px;
     line-height: 24px;
   }
@@ -185,6 +205,11 @@ export default {};
   }
 
   .footer__copyright {
+    font-size: 16px;
+    line-height: 18px;
+  }
+
+  .footer__copyright-link {
     font-size: 16px;
     line-height: 18px;
   }
@@ -209,17 +234,21 @@ export default {};
   }
 }
 @media (max-width: 768px) {
-  .menu__items {
+  .footer__column-links {
     flex-direction: column;
   }
 
-  .menu__item {
+  .footer-nav {
+    display: flex;
+  }
+
+  .footer__list-item {
     padding-bottom: 14px;
     font-size: 16px;
     line-height: 24px;
   }
 
-  .menu__link {
+  .footer__link {
     font-size: 16px;
     line-height: 24px;
   }
@@ -241,12 +270,18 @@ export default {};
     font-size: 13px;
     line-height: 18px;
   }
+
+  .footer__copyright-link {
+    font-size: 13px;
+    line-height: 18px;
+  }
+
   .footer__project {
     font-size: 13px;
     line-height: 18px;
   }
 
-  .menu__items {
+  .footer__column-links {
     padding: 0;
   }
 
