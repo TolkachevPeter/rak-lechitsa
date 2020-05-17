f5ab025a82d24952efd462d7594237176244faa5
AntonZ <antonzm34@gmail.com>
2020-05-17 11:08:06 +0400

    contact popup

---
+++ pages/anton.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <layout />
+    <popup />
+  </div>
+</template>
+
+<script>
+import ContactPopup from '@/components/ContactsPopup';
+import PopupLayout from '@/components/PopupLayout';
+export default {
+  components: {
+    popup: ContactPopup,
+    layout: PopupLayout,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
